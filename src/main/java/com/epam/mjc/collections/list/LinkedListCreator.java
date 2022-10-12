d071cf1981fb7b9e64afb0f4b44acf2cbd4dc895
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-10-12 07:22:51 +0500

    Update LinkedListCreator.java

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,14 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+
+        LinkedList<Integer> res = new LinkedList<>();
+
+        for (Integer integer : sourceList) {
+            if (integer % 2 != 0) res.addFirst(integer);
+            else res.addLast(integer);
+        }
+
+        return res;
     }
 }
