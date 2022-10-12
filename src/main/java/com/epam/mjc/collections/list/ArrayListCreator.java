6dfdb2512392baceed0ea62f097c47dc9daa08f2
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-10-12 07:22:35 +0500

    Update ArrayListCreator.java

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,18 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+
+        int c = 0;
+
+        ArrayList<String> res = new ArrayList<>();
+
+        for (String s : sourceList) {
+            c++;
+            if (c % 3 == 0) {
+                res.add(s);
+                res.add(s);
+            }
+        }
+        return res;
     }
 }
